ca12c57d7f67cbcd8338186f9352a94545c92e74
ypapy <ypapy22@gmail.com>
2020-11-10 10:22:19 +0200

    Nothing to mention

--- js/article.js
+++ js/article.js
@@ -5,8 +5,11 @@ document.querySelector('.submit').addEventListener('click', (e) => {
     e.preventDefault();
     db.collection('article').add({
         article_title: form.articleTitle.value,
-        content: form.content.value
+        content: form.content.value,
+        publishDate: form.publicdate.value
     });
     form.articleTitle.value ='';
     form.content.value ='';
+    form.publicdate.value = '';
+    window.location.assign('edit');
 });
