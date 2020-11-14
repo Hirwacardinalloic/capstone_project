2359facf3a5317cd785d066797341347a7dcb488
ypapy <ypapy22@gmail.com>
2020-11-14 12:31:53 +0200

    Some other changes

---
+++ js/auth.js
@@ -0,0 +1,43 @@
+//Listen for authentication status
+auth.onAuthStateChanged(user => {
+    if(user){
+        
+    }else {
+       
+    }
+});
+
+// const signupForm = document.getElementById('signup-form');
+
+// signupForm.addEventListener('submit', (e) => {
+//     e.preventDefault();
+     
+//     const email = document.getElementById('signup_email').value;
+//     const password = document.getElementById('signup_password').value;
+    
+
+//     //sign up user
+//     auth.createUserWithEmailAndPassword(email, password).then( cred =>{
+//         console.log(cred);
+//     });
+    
+// });
+// //sign out 
+// // const signOutBtn = document.getElementById('sign-out');
+// // signOutBtn.addEventListener('click', (e) => {
+// //     e.preventDefault();
+// //     auth.signOut().then(() => {
+// //         console.log("user signed out");
+// //     });
+// // });
+
+//Sign in
+document.getElementById('signin-form').addEventListener('submit', (e) =>{
+    e.preventDefault();
+    const email = document.getElementById('signin_email').value;
+    const password = document.getElementById('signin_password').value;
+
+    auth.signInWithEmailAndPassword(email, password).then(cred => {
+        
+    });
+});
